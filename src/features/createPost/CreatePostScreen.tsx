452ef91259c8a7d1507f5912804513d74da44167
IgorKiselev1993 <bujhmrbctktd1313@gmail.com>
2025-06-04 21:32:33 +0300

    Requesting application permissions

--- src/features/createPost/CreatePostScreen.tsx
+++ src/features/createPost/CreatePostScreen.tsx
@@ -1,31 +1,63 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text, TouchableOpacity, Image} from 'react-native';
 import {NavigationButton} from '../../component/ui/button/NavigationButton.tsx';
 import {useNavigation} from '@react-navigation/native';
 import {Colors} from '../../constant/colors.ts';
 
 export const CreatePostScreen = () => {
-    const navigation = useNavigation();
-    return (
-      <View style={styles.containerCreatePost}>
-        <Text>Hello</Text>
-          <View style={styles.containerButton}>
-          <NavigationButton styleStates={styles.disabledButton} label={'Submit'} onPress={()=> navigation.goBack()} />
-          </View>
+  const navigation = useNavigation();
+  return (
+    <View style={styles.containerCreatePost}>
+      <View style={styles.containerDataInput}></View>
+      <View style={styles.containerPhotoPicker}>
+        <Text style={styles.textPhoto}>Photo</Text>
+        <TouchableOpacity style={styles.addPhoto}>
+          <Image source={require('../../assets/icons/AddPhoto.png')} />
+        </TouchableOpacity>
       </View>
+      <View style={styles.containerButton}>
+        <NavigationButton
+          label={'Submit'}
+          disabled={true}
+          onPress={() => navigation.goBack()}
+          styleStates={styles.disabledButton}
+        />
+      </View>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   containerCreatePost: {
     flex: 1,
+    backgroundColor: Colors.whitesmoke,
   },
-    containerButton: {
-        flex: 1,
-        justifyContent: 'flex-end',
-        bottom: 30,
-    },
-    disabledButton: {
-      backgroundColor: Colors.gray,
-    },
+  containerButton: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    bottom: 30,
+  },
+  disabledButton: {
+    backgroundColor: Colors.gray,
+  },
+  containerDataInput: {
+    flex: 1,
+  },
+  containerPhotoPicker: {
+    flex: 1,
+    backgroundColor: Colors.white,
+  },
+  textPhoto: {
+    margin: 20,
+    fontSize: 18,
+  },
+  addPhoto: {
+    marginHorizontal: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+    width: 70,
+    height: 70,
+    backgroundColor: Colors.white,
+  },
 });
---
+++ src/utils/PremissionsApp.ts
@@ -0,0 +1,57 @@
+import {Alert, Permission, PermissionsAndroid, Platform} from 'react-native';
+
+export const getPermissions = () =>
+  new Promise(async (resolve, reject) => {
+    if (Platform.OS === 'android' && Platform.Version > 22) {
+      try {
+        const camera = await requestPermission(
+          PermissionsAndroid.PERMISSIONS.CAMERA,
+          'FirstProject App Camera Permission',
+          'FirstProject App needs access to your camera ' +
+            'so you can take  pictures.',
+        );
+
+        const sendSms = await requestPermission(
+          PermissionsAndroid.PERMISSIONS.SEND_SMS,
+          'FirstProject App SEND_SMS',
+          'FirstProject App needs access to your SEND_SMS',
+        );
+
+        const granted =
+          camera === PermissionsAndroid.RESULTS.GRANTED &&
+          sendSms === PermissionsAndroid.RESULTS.GRANTED;
+
+        if (granted) {
+          Alert.alert('Доступ разрешён');
+          console.log('You can use the camera');
+          resolve(true);
+        } else {
+          Alert.alert('Отказано в доступе');
+          console.log('Camera permission denied');
+          resolve(false);
+        }
+      } catch (err) {
+        Alert.alert('Ошибка');
+        console.error(err);
+        reject(err);
+        return;
+      }
+    } else {
+      resolve(true);
+    }
+  });
+
+const requestPermission = async (
+  permission: Permission,
+  title: string,
+  message: string,
+) => {
+  console.info('Requesting permission for:', permission);
+  return await PermissionsAndroid.request(permission, {
+    title,
+    message,
+    buttonPositive: 'OK',
+    buttonNegative: 'Cancel',
+    buttonNeutral: 'Ask Me Later',
+  });
+};
