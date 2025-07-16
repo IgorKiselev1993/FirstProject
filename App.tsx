ba4174a61fc013358383602b97daedf5ad80fcaa
IgorKiselev1993 <bujhmrbctktd1313@gmail.com>
2025-07-16 10:02:48 +0300

    linter-space

--- App.tsx
+++ App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import {RootStack} from './src/navigation/stack/root/RootStackContainer.tsx';
-import { persistor, store} from './src/store/store.ts';
-import { Provider } from 'react-redux';
+import {persistor, store} from './src/store/store.ts';
+import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import {Text} from 'react-native';
 
-export default function App() {
+export default function App(){
   return (
-      <Provider store={store}>
-        <PersistGate loading={<Text>Loading...</Text>} persistor={persistor}>
-      <RootStack />
-        </PersistGate>
-      </Provider>
+    <Provider store={store}>
+      <PersistGate loading={<Text>Loading...</Text>} persistor={persistor}>
+        <RootStack />
+      </PersistGate>
+    </Provider>
   );
 }
